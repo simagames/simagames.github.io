529979e1bd68da47165830670a43e66755a67aed
sma <flsiemer@gmail.com>
2021-04-06 15:40:04 +0200

    * Page update

---
+++ games/mole-mine-assets/molemine/language.js
@@ -0,0 +1,88 @@
+/*
+	MOLE MINE
+	by Florian Siemer, August 2014
+	
+	Keep all of those texts as short as possible,
+	to prevent the interface from looking ugly. 
+	:-)
+*/
+
+var txts = new Object();
+var lang = "en";
+
+txts["en"] = {
+	shop_texts: [
+		"+2% walk speed\n"
+		, "-1% vertical speed\n"
+		, "-2.5% stretch platform\n"
+		, "-2.5% conveyor\n"
+		, "-2.5% speed-up platform\n"
+		, "-2.5% trampoline\n"
+	],
+	arrow: "<==",
+	depth: "Depth: ",
+	highscore: "Highscore: ",
+	no_highscore: "No Highscore",
+	new_highscore: "New Highscore: ",
+	finish_level: "Finish Level",
+	gems: "Gems",
+	costs: "Costs: ",
+	bonus: "Bonus: ",
+	maximum: "MAX",
+	victory: "Victory",
+	game_over: "Game Over",
+	pause: "Pause",
+	wait_text: "Ads, please wait... "
+};
+
+txts["de"] = {
+	shop_texts: [
+		"+2% Bewegungstempo\n"
+		, "-1% vertikales Tempo\n"
+		, "-2.5% Dehn-Plattform\n"
+		, "-2.5% Fließband\n"
+		, "-2.5% Tempo-Plattform\n"
+		, "-2.5% Trampolin\n"
+	],
+	arrow: "<==",
+	depth: "Tiefe: ",
+	highscore: "Highscore: ",
+	no_highscore: "Keine Highscore",
+	new_highscore: "Neue Highscore: ",
+	finish_level: "Beende Level",
+	gems: "Gems",
+	costs: "Kosten: ",
+	bonus: "Bonus: ",
+	maximum: "MAX",
+	victory: "Gewonnen",
+	game_over: "Verloren",
+	pause: "Pause",
+	wait_text: "Werbung, bitte warten... "
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
